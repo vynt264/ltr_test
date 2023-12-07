ff5454156f65273ad14b590440d7cf811c993eb4
vynt-gcode <vynt.gcode@gmail.com>
2023-12-07 09:34:52 +0700

    fix bug around number

--- src/components/orders/orders.service.ts
+++ src/components/orders/orders.service.ts
@@ -140,7 +140,7 @@ export class OrdersService {
 
     return {
       currentTurnIndex: `${(new Date()).toLocaleDateString()} - ${times}`,
-      currentSeconds: secondsInCurrentRound,
+      currentSeconds: Math.round(secondsInCurrentRound),
     };
   }
 
