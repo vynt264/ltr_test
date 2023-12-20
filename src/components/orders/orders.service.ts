9ba84985f8fe7fafe27cb8415be43682293a20df
vynt-gcode <vynt.gcode@gmail.com>
2023-12-20 13:06:15 +0700

    fix bug api get turn index

--- src/components/orders/orders.service.ts
+++ src/components/orders/orders.service.ts
@@ -260,6 +260,17 @@ export class OrdersService {
       nextTime: openTime + (parseInt(query.seconds) * 1000),
       currentSeconds: Math.round(secondsInCurrentRound),
       currentMillisecond: toDate,
+      awardDetail: {
+        0: ['764481'],
+        1: ['03599'],
+        2: ['03853'],
+        3: ['04880', '01115'],
+        4: ['01600', '01021', '01334', '02555', '04047', '05806', '04840'],
+        5: ['3403'],
+        6: ['2681', '8304', '1287'],
+        7: ['737'],
+        8: ['50'],
+      },
     };
   }
 
