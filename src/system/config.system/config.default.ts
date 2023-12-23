8b3be3f8f4760c309ef170e5db7b9e3f00141eb5
hendrick909 <adamhendrick909@gmail.com>
2023-12-23 10:58:44 +0700

    update config

--- src/system/config.system/config.default.ts
+++ src/system/config.system/config.default.ts
@@ -7,5 +7,5 @@ export const DEFAULT_CONFIG: ConfigData = {
   gatekeeperServiceUrl: undefined,
 };
 
-
-export const FE_URL = 'http://192.168.120.17:4000';
+export const FE_URL_1 = 'http://vntop.game.game8b.com';
+export const FE_URL_2 = 'http://supper.rich.lottery.game8b.com';
