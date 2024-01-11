280f061c4d798937b3627302a7b75055a7479ec7
hendrick909 <adamhendrick909@gmail.com>
2024-01-11 17:46:46 +0700

    hot fix

--- src/components/wallet.inout/wallet.inout.module.ts
+++ src/components/wallet.inout/wallet.inout.module.ts
@@ -8,10 +8,11 @@ import { WalletInoutService } from "./wallet.inout.service";
 import { SysConfig } from "../sys.config/sys.config.entity";
 import { SysConfigsModule } from "../sys.config/sys.config.module";
 import { ConnectModule } from "../connect/connect.module";
+import { WalletHistory } from "../wallet/wallet.history.entity";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([WalletInout]),
+    TypeOrmModule.forFeature([WalletInout, WalletHistory]),
     BacklistModule,
     UserModule,
     SysConfigsModule,
