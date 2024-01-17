f19d2e51aea3384b97161f3fe8df81480e301b69
hendrick909 <adamhendrick909@gmail.com>
2024-01-17 18:03:06 +0700

    push code

--- src/components/admin.dashboard/admin.dashboard.service.ts
+++ src/components/admin.dashboard/admin.dashboard.service.ts
@@ -26,13 +26,36 @@ export class AdminDashboardService {
   async dataChartUser(paginationDto: PaginationQueryDto) {
     try {
       const object: any = JSON.parse(paginationDto.keyword);
-      let condition = "entity.usernameReal = '' && entity.role = 'member'";
+      let condition = "entity.usernameReal = '' AND entity.role = 'member'";
       const conditionParams: any = {}
       if (object?.bookmakerId > 0) {
         condition = condition.concat(` AND bookmaker.id = :bookmarkerFind`);
         conditionParams.bookmarkerFind = object?.bookmakerId;
       }
-      const addSelectDateFm = `DATE_FORMAT(entity.createdAt, "%Y-%m-%d") as timeFilter`;
+      if (object?.startDate) {
+        const startDate = new Date(object.startDate);
+        condition = condition.concat(` AND (entity.createdAt >= :timeStart)`);
+        conditionParams.timeStart = startOfDay(startDate);
+      }
+      if (object?.endDate) {
+        const endDate = new Date(object.endDate);
+        condition = condition.concat(` AND (entity.createdAt <= :timeEnd)`);
+        conditionParams.timeEnd = endOfDay(endDate);
+      }
+      if (object?.startDate && object?.endDate) {
+        const startDate = new Date(object.startDate);
+        const endDate = new Date(object.endDate);
+        condition = condition.concat(` AND (entity.createdAt BETWEEN :timeStart AND :timeEnd)`);
+        conditionParams.timeStart = startOfDay(startDate);
+        conditionParams.timeEnd = endOfDay(endDate);
+      }
+
+      const addSelectDateFm = 
+        object?.typeFilter == "day" ?
+        `DATE_FORMAT(entity.createdAt, "%Y-%m-%d") as timeFilter` :
+        object?.typeFilter == "month" ?
+        `DATE_FORMAT(entity.createdAt, "%Y-%m") as timeFilter` : 
+        `DATE_FORMAT(entity.createdAt, "%Y") as timeFilter`;
 
       const listUser = await this.userRepository
         .createQueryBuilder("entity")
