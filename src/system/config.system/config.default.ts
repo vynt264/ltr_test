48d381be27567afcfa31bab3f0e0c7fe3030e31c
hendrick909 <adamhendrick909@gmail.com>
2024-03-08 15:28:42 +0700

    push code

--- src/system/config.system/config.default.ts
+++ src/system/config.system/config.default.ts
@@ -2,7 +2,7 @@ import { ConfigData } from "./config.interface";
 
 export const DEFAULT_CONFIG: ConfigData = {
   env: "",
-  port: 3000,
+  port: 4000,
   logLevel: "info",
   gatekeeperServiceUrl: undefined,
 };
