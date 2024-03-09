f23b6fba47bf505781554e0b10039eea91216550
nguyentrongvy <nguyentrongvy151995@gmail.com>
2024-03-09 13:59:24 +0700

    prevent login multi devide

--- src/components/backlist/backlist.guard.ts
+++ src/components/backlist/backlist.guard.ts
@@ -1,9 +1,9 @@
 import { BacklistService } from "./backlist.service";
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, HttpException, HttpStatus, Injectable } from "@nestjs/common";
 
 @Injectable()
 export class BacklistGuard implements CanActivate {
-  constructor(private backListService: BacklistService) {}
+  constructor(private backListService: BacklistService) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
@@ -16,7 +16,12 @@ export class BacklistGuard implements CanActivate {
         .trim();
       const accepted = await this.backListService.getOneByToken(id, acToken);
       if (accepted) {
-        return false;
+        throw new HttpException(
+          {
+            message: 'LoggedOnAnotherDevice',
+          },
+          HttpStatus.BAD_REQUEST,
+        );
       }
 
       return true;
